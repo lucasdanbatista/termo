59e03de83a4d9dbf58cdb26a3084025f3e86af3a
Lucas Batista <lucasdanbatista@hotmail.com>
2024-02-24 11:46:50 -0300

    Implement term.ooo webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool isLoading = true;
+  final backgroundColor = const Color(0xFF6E5C62);
+  late final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(backgroundColor)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (progress) {
+          if (progress == 100) {
+            setState(() => isLoading = false);
+          }
+        },
+      ),
+    );
+
+  @override
+  void initState() {
+    super.initState();
+    controller.loadRequest(Uri.https('term.ooo'));
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: backgroundColor,
+        ),
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: Colors.white,
+        ),
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Visibility(
+          visible: isLoading,
+          replacement: SafeArea(
+            child: WebViewWidget(
+              controller: controller,
+            ),
+          ),
+          child: const Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       ),
     );
